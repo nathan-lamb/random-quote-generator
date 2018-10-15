9d7e8e104de2d95fdc0a676c8cc74030c567f97d
Nathan Lamb <nathanlamb022@gmail.com>
2018-10-15 21:53:45 +0200

    add action creator and reducer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import * as React from 'react'
 import './App.css'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      author: null,
+      text: null
+    }
+  }
   render () {
     return (
       <div className="App">
