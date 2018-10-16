789d8213d1dc76f3c09b9f718149faade9f261ec
Nathan Lamb <nathanlamb022@gmail.com>
2018-10-16 19:42:47 +0200

    add reducer

--- src/redux.js
+++ src/redux.js
@@ -3,17 +3,21 @@ import { Provider, connect } from 'react-redux'
 import { createStore } from 'redux'
 import App from './App'
 
-const addNewQuote = (quote) => {
+const addNewQuote = (quote, author) => {
   return {
     type: 'NEW_QUOTE',
-    quote
+    quote,
+    author
   }
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
     case 'NEW_QUOTE':
-      return state
+      return {
+        quote: action.quote,
+        author: action.author
+      }
   }
 }
 
